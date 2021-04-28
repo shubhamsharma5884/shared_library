d2bd17278f18b2c7afcbfc41f7436d0a68a47d6a
sharmashubham5884 <shubhamsharma5884@gmail.com>
2021-04-28 21:40:55 +0530

    modified file

--- Jenkinsfile/Appscript.groovy
+++ Jenkinsfile/Appscript.groovy
@@ -2,22 +2,34 @@ pipeline {
 agent any
 stages {
 stage ('build') {
-...
+steps{
+echo 'line 1'
+}
 }
 stage ('test: integration-&-quality') {
-...
+steps{
+echo 'line 2'
+}
 }
 stage ('test: functional') {
-...
+steps{
+echo 'line 3'
+}
 }
 stage ('test: load-&-security') {
-...
+steps{
+echo 'line 4'
+}
 }
 stage ('approval') {
-...
+steps{
+echo 'line 5'
+}
 }
 stage ('deploy:prod') {
-...
+steps{
+echo 'line 6'
+}
 }
 }
 }
